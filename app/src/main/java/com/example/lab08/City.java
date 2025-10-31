6c359b9d3216bd051661fbf4e13317e70539e65c
AlyElathy <aelathy@ualberta.ca>
2025-10-31 14:42:59 -0600

    Pointed out the City method (comment).

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -3,7 +3,7 @@ package com.example.lab08;
 public class City {
     private String city;
     private String province;
-
+    // This is the City method.
     public City(String city, String province){
         this.city = city;
         this.province = province;
